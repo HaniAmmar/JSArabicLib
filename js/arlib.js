449d2942e703c53541b680660f4a285b4d81a771
Hani Ammar <hani.code@outlook.com>
2021-09-24 00:20:03 +0300

    Cleanup

--- js/arlib.js
+++ js/arlib.js
@@ -16,7 +16,7 @@ const ArLib = {
      */
     DisconnectChars: function (text) {
         const len = text.length;
-        let new_text = "";
+        let newText = "";
 
         /*!
          * Arabic Alphabet in the unicode table are betwean 1569 -> 1610 (UTF-16BE: dec).
@@ -28,15 +28,15 @@ const ArLib = {
             if ((cc >= 1569) && (cc <= 1610)) {
                 // Because arrays starts from 0, substract 1569 from char code; to match the index of isolated form.
                 // بحكم أن المصفوفة تبدأ من العدد 0، يطرح 1569 من رقم الحرف؛ للوصول إلى هيئة الحرف المستقلة.
-                if (cc != 1600) { // تخطي المدة.
-                    new_text += String.fromCharCode(this.IsolatedForms[(cc - 1569)]);
+                if (cc !== 1600) { // تخطي المدّة.
+                    newText += String.fromCharCode(this.IsolatedForms[(cc - 1569)]);
                 }
             } else {
-                new_text += text[i];
+                newText += text[i];
             }
         }
 
-        return new_text;
+        return newText;
     },
 
     /*!
@@ -47,7 +47,7 @@ const ArLib = {
      */
     RemoveTashkil: function (text, keepShadda = false) {
         const len = text.length;
-        let new_text = "";
+        let newText = "";
 
         // Remove all Tashkil and (Optional) Shadda.
         // إزالة التشكيل، واختياريًا الشدة.
@@ -58,11 +58,11 @@ const ArLib = {
             // الحروف المستخدمة في التشكيل هي من 1611 إلى 1618.
             // 1617 = ّ
             if (((cc < 1611) || (cc > 1618)) || (keepShadda && (cc === 1617))) {
-                new_text += text[i];
+                newText += text[i];
             }
         }
 
-        return new_text;
+        return newText;
     },
 
     /*!
@@ -85,11 +85,10 @@ const ArLib = {
         return false;
     },
 
-
     /*!
-    * It returns false if Tashkil is not at the end of the word.
-    * تعود بلا إذا كان التشكيل ليس آخر الكلمة.
-    */
+     * It returns false if Tashkil is not at the end of the word.
+     * تعود بلا إذا كان التشكيل ليس آخر الكلمة.
+     */
     CheckLastTashkilForKeeping: function (cc, bcc) {
         if ((cc < 1569) || (cc > 1618)) {
             return true;
@@ -100,7 +99,6 @@ const ArLib = {
         return false;
     },
 
-
     /*!
      * It removes the last Tashkil for every word in a give string if "remove" is set to true,
      * otherwise if keeps only the last Tashkil.
@@ -109,10 +107,11 @@ const ArLib = {
      */
     LastTashkil: function (text, remove = true) {
         const len = text.length;
-        let new_text = "";
+        let newText = "";
 
         if (len !== 0) {
-            let cc = 0, y = 0;
+            let cc = 0;
+            let y = 0;
             let bcc = text.charCodeAt(0);
 
             for (let i = 1; i < len; i++, y++) {
@@ -120,10 +119,10 @@ const ArLib = {
 
                 if (remove) {
                     if (this.CheckLastTashkilForRemoval(cc, bcc)) {
-                        new_text += text[y];
+                        newText += text[y];
                     }
                 } else if (this.CheckLastTashkilForKeeping(cc, bcc)) {
-                    new_text += text[y];
+                    newText += text[y];
                 }
 
                 bcc = cc;
@@ -133,14 +132,14 @@ const ArLib = {
             // آخر حرف.
             if (remove) {
                 if (this.CheckLastTashkilForRemoval(cc, bcc)) {
-                    new_text += text[y];
+                    newText += text[y];
                 }
             } else if (this.CheckLastTashkilForKeeping(cc, bcc)) {
-                new_text += text[y];
+                newText += text[y];
             }
         }
 
-        return new_text;
+        return newText;
     },
 
     /*!
@@ -149,31 +148,32 @@ const ArLib = {
      */
     RemoveTatweel: function (text) {
         const len = text.length;
-        let new_text = "";
+        let newText = "";
 
         for (let i = 0; i < len; i++) {
             // 1600 = Tatweel ـ
             // 1600 = حرف التطويل ـ
             if (text.charCodeAt(i) !== 1600) {
-                new_text += text[i];
+                newText += text[i];
             }
         }
 
-        return new_text;
+        return newText;
     },
 
     /*!
-    * Removes Tashkil if it's obvious to pronounce.
-    * حذف التشكبل الواضح نطقًا.
-    */
+     * Removes Tashkil if it's obvious to pronounce.
+     * حذف التشكبل الواضح نطقًا.
+     */
     ReduceTashkil: function (text) {
         const len = text.length;
-        let new_text = "";
+        let newText = "";
 
         if (len !== 0) {
-            let cc = 0, y = 0;
+            let cc = 0;
+            let y = 0;
             let bcc = text.charCodeAt(0);
-            new_text += text[0];
+            newText += text[0];
 
             for (let i = 1; i < len; i++, y++) {
                 cc = text.charCodeAt(i);
@@ -181,89 +181,88 @@ const ArLib = {
                 switch (cc) {
                     case 1618: // Sukun سكون.
                     case 1614: // Fatha فتحة.
-                        {
-                            if (cc === 1614) { // Fatha فتحة.
-                                // Ignore Fatha if it before all types of Alef.
-                                // تجاهل الفتحة التي تأتي قبل أي ألف.
-                                const n = (i + 1);
-                                if (n < len) {
-                                    const acc = text.charCodeAt(n);
-                                    // 1570 = آ // Alef with Madda above.
-                                    // 1571 = أ // Alef with Hamza above.
-                                    // 1573 = إ // Alef with Hamza below.
-                                    // 1575 = ا // Naked Alef.
-                                    if ((acc === 1570) || (acc === 1571) || (acc === 1573) || (acc === 1575)) {
-                                        break;
-                                    }
-                                }
-                            }
-
-                            // Ignore Fatha and Sukun if it's not on Waw or Yeh, or it's at the beginning of a word.
-                            // تجاهل الفتحة والسكون التي ليست على واوٍ أو ياءٍ، أو أنها في بداية الكلمة
-                            // 1608 = و Waw.
-                            // 1610 = ي Yeh.
-                            if (((bcc === 1608) || (bcc === 1610)) && (y > 0)) {
-                                const acc = text.charCodeAt(i - 2);
-                                if ((acc >= 1569) && (acc <= 1618)) {
-                                    new_text += text[i];
-                                }
-                            }
-
-                            break;
-                        }
-
-                    case 1615: // Damma ضمة.
-                        // Ignore Damma if comes before Waw or with Hamza above, or it on Waw.
-                        // تجاهل الضمة التي بعدها واوٍ أو واوٍ عليها همزة أو على واو.
-                        {
+                    {
+                        if (cc === 1614) { // Fatha فتحة.
+                            // Ignore Fatha if it before all types of Alef.
+                            // تجاهل الفتحة التي تأتي قبل أي ألف.
                             const n = (i + 1);
                             if (n < len) {
                                 const acc = text.charCodeAt(n);
-
-                                // 1608 = و Waw.
-                                // 1572 = ؤ Waw or with Hamza above.
-                                if ((acc === 1608) || (acc === 1572) || (bcc === 1608) || (bcc === 1572)) {
+                                // 1570 = آ // Alef with Madda above.
+                                // 1571 = أ // Alef with Hamza above.
+                                // 1573 = إ // Alef with Hamza below.
+                                // 1575 = ا // Naked Alef.
+                                if ((acc === 1570) || (acc === 1571) || (acc === 1573) || (acc === 1575)) {
                                     break;
                                 }
                             }
-
-
-                            // Ignore Damma if is on Shadda, and Shadda is on Waw (with Hamza above or without).
-                            // تجهاهل الضمة إذا كانت على شدة والشدة على واو، سواء كان على الواو همزة أو لا.
-                            if ((bcc === 1617) && (i > 2)) {
-                                const acc = text.charCodeAt(i - 2);
-                                if ((acc === 1608) || (acc === 1572)) {
-                                    break;
-                                }
-                            }
-
-                            new_text += text[i];
-                            break;
                         }
+
+                        // Ignore Fatha and Sukun if it's not on Waw or Yeh, or it's at the beginning of a word.
+                        // تجاهل الفتحة والسكون التي ليست على واوٍ أو ياءٍ، أو أنها في بداية الكلمة
+                        // 1608 = و Waw.
+                        // 1610 = ي Yeh.
+                        if (((bcc === 1608) || (bcc === 1610)) && (y > 0)) {
+                            const acc = text.charCodeAt(i - 2);
+                            if ((acc >= 1569) && (acc <= 1618)) {
+                                newText += text[i];
+                            }
+                        }
+
+                        break;
+                    }
+
+                    case 1615: // Damma ضمة.
+                    // Ignore Damma if comes before Waw or with Hamza above, or it on Waw.
+                    // تجاهل الضمة التي بعدها واوٍ أو واوٍ عليها همزة أو على واو.
+                    {
+                        const n = (i + 1);
+                        if (n < len) {
+                            const acc = text.charCodeAt(n);
+
+                            // 1608 = و Waw.
+                            // 1572 = ؤ Waw or with Hamza above.
+                            if ((acc === 1608) || (acc === 1572) || (bcc === 1608) || (bcc === 1572)) {
+                                break;
+                            }
+                        }
+
+                        // Ignore Damma if is on Shadda, and Shadda is on Waw (with Hamza above or without).
+                        // تجهاهل الضمة إذا كانت على شدة والشدة على واو، سواء كان على الواو همزة أو لا.
+                        if ((bcc === 1617) && (i > 2)) {
+                            const acc = text.charCodeAt(i - 2);
+                            if ((acc === 1608) || (acc === 1572)) {
+                                break;
+                            }
+                        }
+
+                        newText += text[i];
+                        break;
+                    }
 
                     case 1616: // Kasra كسرة.
-                        {
-                            // Ignore Kasra if comes before Yeh.
-                            // تجاهل الكسرة إذا كان بعدها ياء.
-                            const n = (i + 1);
-                            if (n < len) {
-                                // 1610 = ي Yeh.
-                                if (text.charCodeAt(n) === 1610) {
-                                    break;
-                                }
+                    {
+                        // Ignore Kasra if comes before Yeh.
+                        // تجاهل الكسرة إذا كان بعدها ياء.
+                        const n = (i + 1);
+                        if (n < len) {
+                            // 1610 = ي Yeh.
+                            if (text.charCodeAt(n) === 1610) {
+                                break;
                             }
-
-                            // Ignore Kasra if it's under Alef with Hamza below.
-                            // تجاهل الكسرة التي تحت الألف التي تحتها همزة.
-                            if (bcc !== 1573) {
-                                new_text += text[i];
-                            }
-
-                            break;
                         }
 
+                        // Ignore Kasra if it's under Alef with Hamza below.
+                        // تجاهل الكسرة التي تحت الألف التي تحتها همزة.
+                        if (bcc !== 1573) {
+                            newText += text[i];
+                        }
+
+                        break;
+                    }
+
                     default:
-                        new_text += text[i];
+                        newText += text[i];
                         break;
                 }
 
@@ -271,11 +270,11 @@ const ArLib = {
             }
         }
 
-        return new_text;
+        return newText;
     },
 
     //  ﺀ ﺁ ﺃ ﺅ ﺇ ﺉ ﺍ ﺏ ﺓ ﺕ ﺙ ﺝ ﺡ ﺥ ﺩ ﺭ ﺯ ﺱ ﺵ ﺹ ﺽ ﻁ ﻅ ﻉ ﻍ ﻑ ﻕ ﻙ ﻝ ﻡ ﻥ ﻩ ﻭ ﻯ ﻱ
-    // 1600 = ـ مدة
+    // 1600 = ـ مدّة
     // ػ = ﻙ
     // ؼ = ﻙ
     // ﻯ = ؽ ؾ ؿ
@@ -285,5 +284,3 @@ const ArLib = {
         65189, 65193, 65195, 65197, 65199, 65201, 65205, 65209, 65213, 65217, 65221, 65225,
         65241, 65241, 65263, 65263, 65263, 1600, 65229, 65233, 65237, 65241, 65245, 65249, 65253, 65257, 65261, 65263, 65265]
 };
-
-
